80b8a11562d8051b37faf4b8f92a83ccd7203f17
zach chih <zach.chih@advantech.com.tw>
2020-02-13 14:29:36 +0800

    up

---
+++ 5-output.filename option/webpack-first-project/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // this is actually the configuration webpack uses
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '../build/build.js'
+    }
+}
